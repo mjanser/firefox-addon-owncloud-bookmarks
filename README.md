b35779ef901d04314480f0f1d36e1d9c3ac5595c
Martin Janser <martin@duss-janser.ch>
2016-06-30 16:41:53 +0200

    Use promises, update interval and decode bookmark urls

--- README.md
+++ README.md
@@ -4,9 +4,11 @@ Add-on for syncing bookmarks with ownCloud.
 For now the synchronization is only one-way, from ownCloud to Firefox.
 This means all bookmarks in the configured folder will be overwritten with the ones from ownCloud.
 
+This add-on won't work well if Firefox Sync is enabled, the browser will be unresponsive during the bookmark synchronization.
+
 The password is retrieved from the password store. The easiest way to fill it is:
 
 - Enable saving passwords in Firefox preferences
 - Go to your ownCloud
-- Log in with you user
+- Log in with your user
 - Save the password
--- index.js
+++ index.js
@@ -4,32 +4,123 @@ var events = require("sdk/system/events");
 var preferences = require("sdk/simple-prefs").prefs;
 var sp = require("sdk/simple-prefs");
 var Request = require("sdk/request").Request;
+const { defer, all } = require('sdk/core/promise');
 
-var { setInterval } = require("sdk/timers");
+var { setInterval, clearInterval } = require("sdk/timers");
 let { Bookmark, Group, search, save, remove, MENU } = require("sdk/places/bookmarks");
 
+var interval = null;
+var intervalId = null;
+
+function searchCredentials() {
+    var deferred = defer();
+    var search = {
+        url: preferences.url,
+        onComplete: function (credentials) {
+            if (credentials.length > 0) {
+                deferred.resolve(credentials[0]);
+            } else {
+                deferred.reject("no credentials found in password store for "+preferences.url);
+            }
+        },
+        onError: function (error) {
+            deferred.reject(error);
+        }
+    };
+    if (preferences.username) {
+        search.username = preferences.username;
+    }
+
+    require("sdk/passwords").search(search);
+
+    return deferred.promise;
+}
+
 function getGroup(callback) {
+    var deferred = defer();
     search({ type: "group", group: MENU, title: preferences.group }).on("end", function (results) {
         var group;
-        console.log("found items: "+results.length);
         results.forEach(function (item) {
             if (item.type == "group" && item.title == preferences.group) {
                 group = item;
-                callback(group);
+                deferred.resolve(group);
             }
         });
 
         if (!group) {
-            console.log("creating new group: "+preferences.group);
+            console.log("creating new bookmark group: "+preferences.group);
             group = Group({ title: preferences.group, group: MENU });
             save(group).on("end", function () {
                 callback(group);
+                deferred.resolve(group);
+            }).on("error", function (error) {
+                deferred.reject(error);
             });
         }
+    }).on("error", function (error) {
+        deferred.reject(error);
+    });
+
+    return deferred.promise;
+}
+
+function getBookmarks(username, password) {
+    var deferred = defer();
+    var bookmarksRequest = Request({
+        url: preferences.url + "/index.php/apps/bookmarks/public/rest/v1/bookmark?user=" + username + "&password=" + password + "&select[0]=tags&select[1]=description",
+        onComplete: function (response) {
+            if (200 == response.status) {
+                console.log("fetched "+response.json.length+" bookmarks from "+preferences.url);
+                deferred.resolve(response.json);
+            } else {
+                deferred.reject("fetching bookmarks from "+preferences.url+" returned status "+response.status);
+            }
+        }
+    }).get();
+
+    return deferred.promise;
+}
+
+function syncBookmarks(username, password, group) {
+    getBookmarks(username, password).then(function (ownCloudBookmarks) {
+        // Remove bookmarks
+        search({ group: group }).on("end", function (results) {
+            var oldBookmarks = results.filter(function (item) {
+                return item.type == "bookmark" && item.group.id == group.id;
+            });
+            save(remove(oldBookmarks)).on("end", function () {
+                // Add bookmarks
+                let bookmarks = [];
+                ownCloudBookmarks.forEach(function (ownCloudBookmark) {
+                    var tags = ownCloudBookmark.tags.filter(function (tag) {
+                        return tag != "";
+                    });
+
+                    let bookmark = Bookmark({
+                        title: ownCloudBookmark.title,
+                        url: decodeURI(ownCloudBookmark.url),
+                        tags: tags,
+                        group: group
+                    });
+
+                    bookmarks.push(bookmark);
+                });
+
+                save(bookmarks);
+            });
+        });
     });
 }
 
-function whenReady(callback) {
+function updateInterval() {
+    if (interval != preferences.interval) {
+        clearInterval(intervalId);
+        interval = preferences.interval;
+        intervalId = setInterval(run, interval * 1000 * 60);
+    }
+}
+
+function run() {
     if (!preferences.url || preferences.url == "https://" || !urls.isValidURI(preferences.url)) {
         console.error("url is not valid: "+preferences.url)
 
@@ -42,64 +133,16 @@ function whenReady(callback) {
         return;
     }
 
-    var search = {
-        url: preferences.url,
-        onComplete: function onComplete(credentials) {
-            if (credentials.length > 0) {
-                getGroup(function (group) {
-                    callback(credentials[0].username, credentials[0].password, group);
-                });
-            } else {
-                console.error("no credentials found for "+preferences.url);
-            }
-        }
-    };
-    if (preferences.username) {
-        search.username = preferences.username;
+    try {
+        all([searchCredentials(), getGroup()]).then(function (result) {
+            syncBookmarks(result[0].username, result[0].password, result[1]);
+            updateInterval();
+        });
+    } catch (e) {
+        console.error(e);
+        updateInterval();
     }
-
-    require("sdk/passwords").search(search);
-}
-
-function syncBookmarks(ownCloudBookmarks, group) {
-    // Remove bookmarks
-    search({ group: group }).on("end", function (results) {
-        var oldBookmarks = results.filter(function (item) {
-            return item.type == "bookmark" && item.group.id == group.id;
-        });
-        save(remove(oldBookmarks)).on("end", function () {
-            // Add bookmarks
-            let bookmarks = [];
-            ownCloudBookmarks.forEach(function (ownCloudBookmark) {
-                var tags = ownCloudBookmark.tags.filter(function (tag) {
-                    return tag != "";
-                });
-
-                let bookmark = Bookmark({ title: ownCloudBookmark.title, url: ownCloudBookmark.url, tags: tags, group: group });
-
-                bookmarks.push(bookmark);
-            });
-
-            save(bookmarks);
-        });
-    });
-}
-
-function getBookmarks(username, password, group) {
-    var bookmarksRequest = Request({
-        url: preferences.url + "/index.php/apps/bookmarks/public/rest/v1/bookmark?user=" + username + "&password=" + password + "&select[0]=tags&select[1]=description",
-        onComplete: function (response) {
-            console.log("fetched "+response.json.length+" bookmarks from "+preferences.url);
-            syncBookmarks(response.json, group);
-        }
-    }).get();
-}
-
-function run() {
-    whenReady(getBookmarks);
 }
 
 sp.on("sync", run);
-
-run();
-setInterval(run, preferences.interval * 1000 * 60);
+updateInterval();
